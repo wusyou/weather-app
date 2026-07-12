dcd272da3bc2d91f9832af24befef0b7c122b606
wusyou <ambrocio.johnrey1008@gmail.com>
2026-07-12 19:35:36 +0800

    Use process.cwd for vercel path resolution

--- index.js
+++ index.js
@@ -7,14 +7,20 @@ import { fileURLToPath } from "url";
 const app = express();
 const port = 3000;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import express from "express";
+import axios from "axios";
+import "dotenv/config";
+import path from "path";
+
+const app = express();
+const port = 3000;
 
 const apiKey = process.env.WEATHER_API_KEY;
 const weatherAPI = "https://api.weatherapi.com/v1";
 
-app.set("views", path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(process.cwd(), "views"));
+app.set("view engine", "ejs"); // Tahasang sabihin na EJS ang gamit natin
+app.use(express.static(path.join(process.cwd(), "public")));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
